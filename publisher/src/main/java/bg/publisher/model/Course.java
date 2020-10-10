7ee3a528548201334ddbc3e53d8cfe4f8c19f07b
Pavlin Pavlov <pavlin.pavlov@gmail.com>
2020-10-10 19:10:34 +0300

    Added models

---
+++ publisher/src/main/java/bg/publisher/model/Course.java
@@ -0,0 +1,60 @@
+package bg.publisher.model;
+
+import java.time.OffsetDateTime;
+import java.util.List;
+
+public class Course {
+
+    private String name;
+    private int credits;
+    private OffsetDateTime examDate;
+    private List<String> exerciseDescriptions;
+
+    public Course() {
+    }
+
+    public Course(String name, int credits, OffsetDateTime examDate) {
+        this.name = name;
+        this.credits = credits;
+        this.examDate = examDate;
+    }
+
+    public Course(String name, int credits, OffsetDateTime examDate, List<String> exerciseDescriptions) {
+        this.name = name;
+        this.credits = credits;
+        this.examDate = examDate;
+        this.exerciseDescriptions = exerciseDescriptions;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCredits() {
+        return credits;
+    }
+
+    public void setCredits(int credits) {
+        this.credits = credits;
+    }
+
+    public OffsetDateTime getExamDate() {
+        return examDate;
+    }
+
+    public void setExamDate(OffsetDateTime examDate) {
+        this.examDate = examDate;
+    }
+
+    public List<String> getExerciseDescriptions() {
+        return exerciseDescriptions;
+    }
+
+    public void setExerciseDescriptions(List<String> exerciseDescriptions) {
+        this.exerciseDescriptions = exerciseDescriptions;
+    }
+}
